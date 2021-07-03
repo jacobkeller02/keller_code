25d07a8124b514e9bfc99eaf4541399c1bf13a9b
jacobkeller02 <keller02.jk@gmail.com>
2021-07-03 18:42:48 -0400

    Even more refined version of alphabetical_sort

--- Problem.py
+++ Problem.py
@@ -1,26 +1,26 @@
 # write a file that takes in a file name via user input and prints out an the words in alphabetical order but only one instance of each word.
 
 
-def read_in()->str:
+def read_in() -> str:
     """Take in a file and return the contents as one long string"""
     print("Please input a file name: ")
-    user_in=input()
+    user_in = input()
     open_file = open(user_in, "r")
-    contents=open_file.read()
+    contents = open_file.read()
     open_file.close()
     return contents
 
 
-def sort_file(jen: str) -> list:
+def sort_file(param: str) -> list:
     """Splits a string and sorts it into a list with no duplicates"""
-    white_space= (jen.split(" "))
-    sorted_list=sorted(list(set(white_space)))  # Puts B into a set to remove duplicates and then makes it a list and sorts it
+    white_space = param.split(" ")
+    sorted_list = sorted(list(set(white_space)))  # Puts B into a set to remove duplicates and then makes it a list and sorts it
     return sorted_list
 
 
 if __name__ == "__main__":
-    file_contents= read_in()
+    file_contents = read_in()
    
     for word in sort_file(file_contents):
-        print (word)
+        print(word)
     
