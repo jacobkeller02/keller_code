32d077c826fc792b572a17d52dd5f6b511e3ba35
jacobkeller02 <keller02.jk@gmail.com>
2021-07-03 18:25:39 -0400

    Refined promplem.py

--- Problem.py
+++ Problem.py
@@ -1,19 +1,26 @@
-print ("pie")
-print("Jen is a boss")
-#jen is distracted 
-#write a file that takes in a file name via user input and prints out an the words in alphabetical order but only one instance of each word.
-def my_function():
-    """This is a DOCSTRING"""
-    x=input()
-    print (x)
-    f = open(x, "r")
-    b= (f.read().split(" "))
-    d=sorted(list(set(b)))
+# write a file that takes in a file name via user input and prints out an the words in alphabetical order but only one instance of each word.
 
-    for i in d:
-        print (i)
-    #print(g)
+
+def read_in()->str:
+    """Take in a file and return the contents as one long string"""
+    print("Please input a file name: ")
+    user_in=input()
+    open_file = open(user_in, "r")
+    contents=open_file.read()
+    open_file.close()
+    return contents
+
+
+def sort_file(jen: str) -> list:
+    """Splits a string and sorts it into a list with no duplicates"""
+    white_space= (jen.split(" "))
+    sorted_list=sorted(list(set(white_space)))  # Puts B into a set to remove duplicates and then makes it a list and sorts it
+    return sorted_list
+
 
 if __name__ == "__main__":
-    #my_function()
-    assert(==b.sorted)
+    file_contents= read_in()
+   
+    for word in sort_file(file_contents):
+        print (word)
+    
